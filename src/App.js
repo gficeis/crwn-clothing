d3f4ed74bb245762752d039089b54d2f1a8911af
gficeis <gficeis@gmail.com>
2021-01-25 16:05:53 -0300

    Update menu-itens to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+
+)
 
 function App() {
-  return (
+  return ( 
     <div>
-    < HomePage />
+    <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HomePage}/>
+    </Switch>
+    </BrowserRouter>
     </div>
   );
 }
